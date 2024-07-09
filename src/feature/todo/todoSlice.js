55996326d1365962d9252156adeb6813bbc7b430
TechRam09 <nsshreeram@gmail.com>
2024-07-09 23:20:02 +0530

    Create todoSlice.js

---
+++ src/feature/todo/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    todos: [{ id: 1, text: "First slice" }],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
